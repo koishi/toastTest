c68557998a0efd7998266fe56228a9c6c3ee48f4
Koichiro Oishi <bs@v7.com>
2017-11-24 11:50:47 +0900

    1st commit

--- toastTest/ViewController.swift
+++ toastTest/ViewController.swift
@@ -12,14 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func tapped(_ sender: UIButton) {
+        let toastView = UINib(nibName: "toast", bundle: nil).instantiate(withOwner: self, options: nil)[0] as! UIView
+        view.addSubview(toastView)
+        toastView.translatesAutoresizingMaskIntoConstraints = false
+        view.addConstraints([NSLayoutConstraint.init(item: toastView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0),
+                                  NSLayoutConstraint.init(item: toastView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0)])
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            toastView.removeFromSuperview()
+        }
     }
-
-
 }
 
